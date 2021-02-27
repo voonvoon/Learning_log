0232f1b6a6f888bcc23ae8470ab3218d4e9b9f0a
voon220 <peter220@gmail.com>
2021-02-27 13:53:08 +0800

    Set DEBUG=FALSE for Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -151,8 +151,10 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    # Allow only Heroku to host the project.
+    ALLOWED_HOSTS = ['learning-log-220.herokuapp.com']
+
+    DEBUG = FALSE
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
