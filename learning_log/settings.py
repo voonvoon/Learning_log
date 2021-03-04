46f884af438e2d57c01441a4968307cd1f80f549
voon220 <peter220@gmail.com>
2021-03-04 11:44:00 +0800

    add images2.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -128,7 +128,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
 
 # My settings
 LOGOUT_REDIRECT_URL = '/'
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
